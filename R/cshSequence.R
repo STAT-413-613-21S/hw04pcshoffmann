ec6f26088effe7f1bd0e70070b5f7ce0514fc2b6
Christopher Hoffmann <cstephanhoffmann@gmail.com>
2021-03-01 13:31:35 -0500

    added spellcheck and documentation for cshSequence

--- R/cshSequence.R
+++ R/cshSequence.R
@@ -1,3 +1,15 @@
+#' Finds the nth element of a recursive sequence
+#'
+#' The function takes two parameters: a vector of three numbers and an integer denoting the nth element of a recursive
+#' sequence. The function will find the value of the nth element in a specific recursive sequence.
+#'
+#' @param x a vector of three numbers that acts as the beginning of a sequence
+#' @param n an integer denoting the nth element of a sequence
+#'
+#' @return the value of the nth element of the recurse sequence
+#' @export cshSequence
+#'
+#' @examples cshSequence(c(2, 4, 3), 5)
 cshSequence <- function(x, n) {
   stopifnot(is.numeric(x), # vector x must be of numbers
             length(x) == 3, # vector x must be length 3
