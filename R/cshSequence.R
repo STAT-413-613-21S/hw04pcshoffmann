a02f462755bcddbd3186ed0a6afed3271f44a53f
Christopher Hoffmann <cstephanhoffmann@gmail.com>
2021-03-01 13:36:59 -0500

    updated wordlist and fixed typo in /cshSequence

--- R/cshSequence.R
+++ R/cshSequence.R
@@ -6,7 +6,7 @@
 #' @param x a vector of three numbers that acts as the beginning of a sequence
 #' @param n an integer denoting the nth element of a sequence
 #'
-#' @return the value of the nth element of the recurse sequence
+#' @return the value of the nth element of the recursive sequence
 #' @export cshSequence
 #'
 #' @examples cshSequence(c(2, 4, 3), 5)
